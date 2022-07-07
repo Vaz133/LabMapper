4b9d6a7aacb796c210d41b4b082368224cc9c47f
Vaz133 <vahidazimi@macbook-air-7.wusm-wifi.wucon.wustl.edu>
2022-07-07 15:52:39 -0500

    initial commit

--- scripts/a1c_mapping.py
+++ scripts/a1c_mapping.py
@@ -1,50 +1,49 @@
 import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import geoplot as gplt 
 import geoplot.crs as gcrs 
 import mapclassify as mc
 import statistics as stats
 import os
+import docker
+import a1c_states
 
-def load_data(data_path, city, svi=False, city_dir=None):
-    
-	if city == 'St. Louis':
-		tract_path = '../maps/stlouis/tl_2020_29510_tract10.shp'
-		if svi:
-			df_svi = gpd.read_file('../svi/Missouri/SVI2018_MISSOURI_tract.shp')
-			df_svi['NAME10'] = (df_svi['LOCATION'].str.split(',').str[0].str.split('Tract ').str[1]).astype('float')
-	elif city == 'Chicago':
-		tract_path = '../maps/chicago/geo_export_a4ec3105-9b79-40d8-81b0-c8b865cbc38f.shp'
-	elif city == 'New York City':
-		tract_path = '../maps/newyork/nyct2010.shp'
-	elif city == 'Custom':
-		for file in os.listdir(city_dir):
-			if file.endswith('.shp'):
-				tract_path = city_dir + '/' + file
+def perform_geocoding(geo_path):
 
-	df_data = pd.read_csv(data_path)
-	tract_geodf = gpd.read_file(tract_path)
-	tract_geodf['NAME10'] = tract_geodf['NAME10'].astype(float)
-	if svi:
-		tract_geodf = pd.merge(tract_geodf, df_svi.rename(columns={'NAME20':'NAME10'})[['RPL_THEMES', 
-			'E_TOTPOP','E_PCI', 'EP_POV', 'EP_UNEMP', 'EP_PCI', 'EP_NOHSDP', 'EP_AGE65',
-			'EP_AGE17', 'EP_DISABL', 'EP_SNGPNT', 'EP_MINRTY', 'EP_LIMENG', 'EP_MUNIT', 
-			'EP_MOBILE', 'EP_CROWD', 'EP_NOVEH', 'EP_GROUPQ', 'RPL_THEME1',
-			'RPL_THEME2', 'RPL_THEME3', 'RPL_THEME4', 'EP_UNINSUR', 'NAME10']], on='NAME10')
-    
-	return(df_data, tract_geodf)
+	os.system("docker run --rm -v " + '/'.join(os.getcwd().split('/')[:-1]) + ":/tmp " +  "degauss/geocoder:3.0.2 " + geo_path.split('/')[-1])
+
+def load_data(fips_df, state=None, county=None, data_path=None):
+
+	state_abbrev = a1c_states.state_abbrev_dict[state]
+	fips = fips_df[(fips_df['name'] == county) & (fips_df['state'] == state_abbrev)]['fips'].values[0]
+	if ' ' not in state:
+		svi_path = '../svi/' + state + '/SVI2018_' + state.upper() + '_tract.shp'
+	elif ' ' in state:
+		svi_path = '../svi/' + state + '/SVI2018_' + state.split(' ')[0].upper() + state.split(' ')[1].upper() + '_tract.shp'
+
+	tract_geodf = gpd.read_file(svi_path)
+	tract_geodf['STCNTY'] = tract_geodf['STCNTY'].astype(int)
+	tract_geodf_county = tract_geodf[tract_geodf['STCNTY'] == fips]
+
+	if data_path != None:
+		df_data = pd.read_csv(data_path)
+		return(df_data, tract_geodf_county)
+	else:
+		return(tract_geodf_county)
+
 
 ## returns dataframe with census tract for each sample
 def spatial_join(df_data, tract_geodf):
     
     gdf = gpd.GeoDataFrame(
-    df_data, geometry=gpd.points_from_xy(df_data.x, df_data.y)
+    df_data, geometry=gpd.points_from_xy(df_data.lon, df_data.lat)
     )
     gdf.crs = 'epsg:4326'
     gdf = gdf.to_crs('epsg:4269')
     df_tracts_merged = gpd.sjoin(gdf, tract_geodf, how="inner", op="within")
-    
+
     return(df_tracts_merged)
 
 ## returns dataframe of stats for each census tract 
@@ -52,19 +51,19 @@ def calc_stats(df_tracts_merged, tract_geodf, stat_num=None, stat_cat=None, comp
 	comparator2=None, constraint1=None, constraint2=None, conditional=None, 
 	cat=False, num=False, pos_cat=None, neg_cat=None):
     
-	if num:
+	if num == True:
 		df_tracts_merged.result = pd.to_numeric(df_tracts_merged.result, errors='coerce')
 		df_tracts_merged = df_tracts_merged[df_tracts_merged['result'].notna()]
-		tract_summary = pd.DataFrame(columns=['NAME10', 'result', 'count'])
-		grouped = df_tracts_merged.groupby('NAME10')
+		tract_summary = pd.DataFrame(columns=['FIPS', 'result', 'count'])
+		grouped = df_tracts_merged.groupby('FIPS')
 		for name, group in grouped:
 			count = len(group)
 			if stat_num[0] == 'Median':
 				median = stats.median(group['result'])
-				tract_summary = tract_summary.append({'NAME10': name, 'result': median, 'count': count}, ignore_index=True)
+				tract_summary = tract_summary.append({'FIPS': name, 'result': median, 'count': count}, ignore_index=True)
 			elif stat_num[0] == 'Mean':
 				mean = stats.median(group['result'])
-				tract_summary = tract_summary.append({'NAME10': name, 'result': mean, 'count': count}, ignore_index=True)
+				tract_summary = tract_summary.append({'FIPS': name, 'result': mean, 'count': count}, ignore_index=True)
 			elif stat_num[0] == 'Percentage':
 				if len(comparator2) == 0:
 					if comparator1[0] == '>':
@@ -150,29 +149,33 @@ def calc_stats(df_tracts_merged, tract_geodf, stat_num=None, stat_cat=None, comp
 						elif comparator1[0] == '<=' and comparator2[0] == '<=':
 							percentage = len(group[(group['result'] <= float(constraint1)) | (group['result'] <= float(constraint2))])/len(group)
 
-				tract_summary = tract_summary.append({'NAME10': name, 'result': percentage, 'count': count}, ignore_index=True)
-			
-	elif cat:
-		tract_summary = pd.DataFrame(columns=['NAME10', 'result', 'count'])
-		grouped = df_tracts_merged.groupby('NAME10')
+				tract_summary = tract_summary.append({'FIPS': name, 'result': percentage, 'count': count}, ignore_index=True)
+		# tract_summary = pd.merge(tract_summary, tract_geodf, on='FIPS')
+
+	elif cat == True:
+		tract_summary = pd.DataFrame(columns=['FIPS', 'result', 'count'])
+		grouped = df_tracts_merged.groupby('FIPS')
 		for name, group in grouped:
 			count = len(group)
 			if stat_cat[0] == 'Count':
 				pos_count = len(group[group['result'] == pos_cat])
-				tract_summary = tract_summary.append({'NAME10': name, 'result': pos_count, 'count': count}, ignore_index=True)
+				tract_summary = tract_summary.append({'FIPS': name, 'result': pos_count, 'count': count}, ignore_index=True)
 			elif stat_cat[0] == 'Rate':
 				rate = len(group[group['result'] == pos_cat])/(len(group[group['result'] == pos_cat])+len(group[group['result'] == neg_cat]))
-				tract_summary = tract_summary.append({'NAME10': name, 'result': rate, 'count': count}, ignore_index=True)
+				tract_summary = tract_summary.append({'FIPS': name, 'result': rate, 'count': count}, ignore_index=True)
 
-	tract_summary = pd.merge(tract_summary, tract_geodf, on='NAME10')
+		# tract_summary = pd.merge(tract_summary, tract_geodf, on='FIPS')
 	return(tract_summary)
 
 
-def create_choropleth(tract_summary, tract_geodf, color, svi=False, count=False, scheme=False, k_num=None, ses_var=None, title=None):
+def create_choropleth(tract_geodf, color, ses_var, count=False, grps=False, k_num=None, title=None, tract_summary=None):
 
-	tract_summary = gpd.GeoDataFrame(tract_summary, geometry='geometry')
-	tract_summary.result = pd.to_numeric(tract_summary.result, errors='ignore')
-	tract_summary['count'] = pd.to_numeric(tract_summary['count'], errors='ignore')
+	if tract_summary is not None:
+		tract_summary = pd.merge(tract_summary, tract_geodf, on='FIPS')
+		tract_summary = gpd.GeoDataFrame(tract_summary, geometry='geometry')
+		tract_summary.result = pd.to_numeric(tract_summary.result, errors='ignore')
+		tract_summary['count'] = pd.to_numeric(tract_summary['count'], errors='ignore')
+	# print(tract_summary)
 
 	if color == 'red':
 		Color = 'Reds'
@@ -191,7 +194,7 @@ def create_choropleth(tract_summary, tract_geodf, color, svi=False, count=False,
 	'Age 17 and younger (%)': 'EP_AGE17',
 	'Non-white persons (%)': 'EP_MINRTY',
 	'Per capita income ($)': 'EP_PCI',
-	'Poverty rate': 'EP_POV',
+	'Poverty rate (%)': 'EP_POV',
 	'Unemployment rate (%)': 'EP_UNEMP',
 	'No high school diploma (%)': 'EP_NOHSDP',
 	'With a disability (%)': 'EP_DISABL',
@@ -204,79 +207,180 @@ def create_choropleth(tract_summary, tract_geodf, color, svi=False, count=False,
 	'Living in group quarters (%)': 'EP_GROUPQ',
 	'No health insurance (%)': 'EP_UNINSUR'}
 
-	y = 1
-	map_list = []
-	if svi:
-		map_list.append('svi')
-		y += 1
-	if count:
-		map_list.append('count')
-		y += 1
+	if ses_var != "Select SES Variable":
+		tract_geodf.loc[tract_geodf[ses_var_dict[ses_var]] < 0, ses_var_dict[ses_var]] = tract_geodf[ses_var_dict[ses_var]].median()
 
-	proj = gcrs.AlbersEqualArea(central_longitude=-90.199402, central_latitude=38.627003)
+	y = 0
+	map_tracker = []
+	if ses_var != "Select SES Variable":
+		y += 1
+		map_tracker.append('svi')
+	if count:
+		y += 1
+		map_tracker.append('count')
+	if tract_summary is not None:
+		y += 1
+		map_tracker.append('tract')
+	longitude = tract_geodf.to_crs('+proj=cea').dissolve().centroid.to_crs(tract_geodf.crs).x[0]
+	latitude = tract_geodf.to_crs('+proj=cea').dissolve().centroid.to_crs(tract_geodf.crs).y[0]
+
+	proj = gcrs.AlbersEqualArea(central_longitude=longitude, central_latitude=latitude)
 
 	fig, axarr = plt.subplots(1, y, figsize=(y*3, 5), subplot_kw={
-	'projection': proj})
+	'projection': proj}, sharex=True, sharey=True)
 
-	if len(map_list) >= 1:
-		for i, j in enumerate(map_list):
-			if j == 'svi':
-				gplt.choropleth(tract_geodf,
-				hue=ses_var_dict[ses_var],
-				cmap=Color,
-				legend=True,
-				ax=axarr[i],
-				projection=proj)
-				axarr[i].set_title(ses_var.title(), pad=20)
-			if j == 'count':
-				gplt.choropleth(tract_summary,
-				hue='count',
-				cmap=Color,
-				legend=True,
-				ax=axarr[i],
-				projection=proj)
-				axarr[i].set_title('Sample size per tract', pad=20)
-		if scheme:
-			scheme = mc.Quantiles(tract_summary.result, k=int(k_num))
-			gplt.choropleth(tract_summary,
-			hue='result',
-			cmap=Color,
-			legend=True,
-			ax=axarr[y-1],
-			scheme=scheme,
-			projection=proj)	
-			if title != None:
-				axarr[y-1].set_title(title, pad=20)
-		else:
-			gplt.choropleth(tract_summary,
-			hue='result',
-			cmap=Color,
-			legend=True,
-			ax=axarr[y-1],
-			projection=proj)	
-			if title != None:
-				axarr[y-1].set_title(title, pad=20)
-	else:
-		if scheme:
-			scheme = mc.Quantiles(tract_summary.result, k=int(k_num))
-			gplt.choropleth(tract_summary,
-			hue='result',
-			cmap=Color,
-			legend=True,
-			scheme=scheme,
-			ax=axarr,
-			projection=proj)	
-			if title != None:
-				axarr.set_title(title, pad=20)		
-		else:
-			gplt.choropleth(tract_summary,
-			hue='result',
-			cmap=Color,
-			legend=True,
-			ax=axarr,
-			projection=proj)
-			if title != None:
-				axarr.set_title(title, pad=20)
+	for i, j in enumerate(map_tracker):
+		if len(map_tracker) > 1:
+			axarr[i].format_coord = lambda x, y: ""
+			if grps == False:
+				if j =='svi':
+					gplt.choropleth(tract_geodf,
+					hue=ses_var_dict[ses_var],
+					cmap=Color,
+					legend=True,
+					ax=axarr[i],
+					projection=proj,
+					legend_kwargs={'pad':0.03,
+					'shrink':0.8,
+                             'orientation':'horizontal'})
+					axarr[i].set_title(ses_var.title(), pad=10)
+				if j == 'count':
+					gplt.choropleth(tract_summary,
+					hue='count',
+					cmap=Color,
+					legend=True,
+					ax=axarr[i],
+					projection=proj,
+					legend_kwargs={'pad':0.03,
+					'shrink':0.8,
+                             'orientation':'horizontal'})
+					axarr[i].set_title('Sample size per tract', pad=10)
+				if j == 'tract':
+					gplt.choropleth(tract_summary,
+					hue='result',
+					cmap=Color,
+					legend=True,
+					ax=axarr[i],
+					projection=proj,
+					legend_kwargs={'pad':0.03,
+					'shrink':0.8,
+                             'orientation':'horizontal'})
+					axarr[i].set_title(title, pad=10)
+			elif grps == True:
+				scheme = mc.Quantiles(tract_summary.result, k=int(k_num))
+				cont_labels = []
+				for s in scheme.get_legend_classes():
+				    upper = s.split('.')[2].split(']')[0]
+				    lower = s.split('.')[1].split(',')[0]
+				    cont_labels.append(str(int(lower)) + '-' + str(int(upper)) +'%')
+				if j =='svi':
+					gplt.choropleth(tract_geodf,
+					hue=ses_var_dict[ses_var],
+					cmap=Color,
+					legend=True,
+					ax=axarr[i],
+					projection=proj,
+					legend_kwargs={'pad':0.03,
+					'shrink':0.8,
+                             'orientation':'horizontal'})
+					axarr[i].set_title(ses_var.title(), pad=10)
+				if j == 'count':
+					gplt.choropleth(tract_summary,
+					hue='count',
+					cmap=Color,
+					legend=True,
+					ax=axarr[i],
+					projection=proj)
+					axarr[i].set_title('Sample size per tract', pad=10)
+				if j == 'tract':
+					gplt.choropleth(tract_summary,
+					hue='result',
+					cmap=Color,
+					legend=True,
+					ax=axarr[i],
+					scheme=scheme,
+					legend_kwargs={'loc': 'upper left'},
+					legend_labels=cont_labels,
+					projection=proj)
+					axarr[i].set_title(title, pad=10)
+
+		elif len(map_tracker) == 1:
+			axarr.format_coord = lambda x, y: ""
+			if grps == False:
+				if j =='svi':
+					gplt.choropleth(tract_geodf,
+					hue=ses_var_dict[ses_var],
+					cmap=Color,
+					legend=True,
+					ax=axarr,
+					projection=proj,
+					legend_kwargs={'pad':0.03,
+					'shrink':0.8,
+                             'orientation':'horizontal'})
+					axarr.set_title(ses_var.title(), pad=10)
+				if j == 'count':
+					gplt.choropleth(tract_summary,
+					hue='count',
+					cmap=Color,
+					legend=True,
+					ax=axarr,
+					projection=proj,
+					legend_kwargs={'pad':0.03,
+					'shrink':0.8,
+                             'orientation':'horizontal'})
+					axarr.set_title('Sample size per tract', pad=10)
+				if j == 'tract':
+					gplt.choropleth(tract_summary,
+					hue='result',
+					cmap=Color,
+					legend=True,
+					ax=axarr,
+					projection=proj,
+					legend_kwargs={'pad':0.03,
+					'shrink':0.8,
+                             'orientation':'horizontal'})
+					axarr.set_title(title, pad=10)
+			elif grps == True:
+				scheme = mc.Quantiles(tract_summary.result, k=int(k_num))
+				cont_labels = []
+				for s in scheme.get_legend_classes():
+				    upper = s.split('.')[2].split(']')[0]
+				    lower = s.split('.')[1].split(',')[0]
+				    cont_labels.append(str(int(lower)) + '-' + str(int(upper)) +'%')
+				if j =='svi':
+					gplt.choropleth(tract_geodf,
+					hue=ses_var_dict[ses_var],
+					cmap=Color,
+					legend=True,
+					ax=axarr,
+					projection=proj,
+					legend_kwargs={'pad':0.03,
+					'shrink':0.8,
+                             'orientation':'horizontal'})
+					axarr.set_title(ses_var.title(), pad=10)
+				if j == 'count':
+					gplt.choropleth(tract_summary,
+					hue='count',
+					cmap=Color,
+					legend=True,
+					ax=axarr,
+					projection=proj,
+					legend_kwargs={'pad':0.03,
+					'shrink':0.8,
+                             'orientation':'horizontal'})
+					axarr.set_title('Sample size per tract', pad=10)
+				if j == 'tract':
+					gplt.choropleth(tract_summary,
+					hue='result',
+					cmap=Color,
+					legend=True,
+					ax=axarr,
+					scheme=scheme,
+					legend_labels=cont_labels,
+					projection=proj,
+					legend_kwargs={'loc': 'upper left'})
+					axarr.set_title(title, pad=10)				
+	plt.show(block=False)
 
 
 
